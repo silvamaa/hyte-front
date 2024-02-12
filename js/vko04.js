e3a44db53f2a0e395972152d4a6b8ac066dd26a2
silvamaa <silvava@metropolia.fi>
2024-02-12 12:55:23 +0200

    On branch vko04 Changes to be committed: 	modified:   css/vko03.css 	new file:   css/vko04.css 	modified:   html/about-us.html 	modified:   html/contact.html 	modified:   html/vko03.html 	new file:   html/vko04.html 	new file:   js/vko04.js

---
+++ js/vko04.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function displayText() {
+  var textInput = document.getElementById("textInput").value;
+  var displayArea = document.getElementById("displayArea");
+  displayArea.textContent = textInput;
+};
